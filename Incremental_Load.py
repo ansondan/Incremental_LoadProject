216a4e971620b3f45e69358a7c50d1c7fe6eb53b
Consultant <adanielansong@gmail.com>
2023-10-03 15:34:24 -0400

    for incremental load in pyspark

--- Incremental_Load.py
+++ Incremental_Load.py
@@ -9,7 +9,7 @@ spark = SparkSession \
 
 dburl="jdbc:postgresql://ec2-3-9-191-104.eu-west-2.compute.amazonaws.com:5432/testdb"
 
-max = spark.sql("select max(row_id) from daniel.fraudproject2 as max")
+max = spark.sql("select max(row_id) from ansong.fraud_data as max")
 
 max = max.first()['max(row_id)']
 
@@ -20,7 +20,9 @@ df = spark.read.format("jdbc").option("url",dburl) \
 
 print(df.show())
 
+df = df.withColumnRenamed("type", "transaction_type")
+
 # Create Hive Internal table
-df.write.mode('append').saveAsTable("daniel.fraudproject2")
+df.write.mode('append').saveAsTable("ansong.fraud_data")
 
 spark.stop()
