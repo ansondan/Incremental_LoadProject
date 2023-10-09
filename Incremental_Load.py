abeacfa9a8e7dee1fcfeef3eb769b71cd1952f1f
Consultant <adanielansong@gmail.com>
2023-10-09 16:17:14 -0400

    fraud data Incremental load

--- Incremental_Load.py
+++ Incremental_Load.py
@@ -9,12 +9,11 @@ spark = SparkSession \
 
 dburl="jdbc:postgresql://ec2-3-9-191-104.eu-west-2.compute.amazonaws.com:5432/testdb"
 
-max = spark.sql("select max(row_id) from ansong.fraud_data2 as max")
+max = spark.sql("select max(row_id) from fraud_project_demo.fraud_full_load_demo as max")
 
 max = max.first()['max(row_id)']
-max += 1
-endmax = max + 10000
-query="(select * from frauddetection where row_id between "+str(max)+" and "+str(endmax)+") as tb"
+
+query="(select * from fraudtable where row_id > "+str(max)+ ") as tb"
 df = spark.read.format("jdbc").option("url",dburl) \
     .option("driver", "org.postgresql.Driver").option("dbtable", query) \
     .option("user", "consultants").option("password", "WelcomeItc@2022").load()
